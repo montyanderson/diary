0b834bbcd128c3b2d37b7cad1f73f6f023ae27da
Monty Anderson <monty.linux@gmail.com>
2016-12-10 21:09:36 +0000

    uses proper encryption

--- addEntry.js
+++ addEntry.js
@@ -1,23 +1,19 @@
 const readline = require("readline");
-const crypto = require("crypto");
+const cryptoWrapper = require("./lib/cryptoWrapper");
 const db = require("./lib/db");
 
 const message = process.argv[2];
-
-console.log(`Adding entry: '${message}'!`);
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
-rl.question("Password to encrypt with: ", (password) => {
-	const cipher = crypto.createCipher("aes-256-cbc", password);
+rl.question("Password to encrypt with: ", password => {
+	const key = cryptoWrapper.deriveKey(password);
+	console.log(`Derived key: ${key.toString("base64")} (${key.length} bytes)`);
 
-	let encryptedMessage = "";
-	encryptedMessage += cipher.update(message, "utf8", "base64");
-	encryptedMessage += cipher.final("base64");
-
+	const encryptedMessage = cryptoWrapper.encrypt(key, message);
 	console.log(`Encrypted Message: ${encryptedMessage}`);
 
 	db.incrAsync("diary-entries")
--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,34 @@
-const crypto = require("crypto");
+const cryptoWrapper = window.cryptoWrapper = require("../lib/cryptoWrapper.js");
+const asmCrypto = window.asmCrypto = require("asmcrypto.js");
 
 const localStorage = window.localStorage || {};
-
-function decryptMessage(password, message) {
-	const decipher = crypto.createDecipher("aes-256-cbc", password);
-
-	let decrypted = "";
-
-	try {
-		decrypted += decipher.update(message, "base64", "utf8");
-		decrypted += decipher.final("utf8");
-	} finally {
-		decrypted = decrypted || null;
-	}
-
-	return decrypted;
-}
+const $ = q => document.querySelectorAll(q);
 
 function decryptAllMessages(password) {
-	document.querySelectorAll(".message").forEach(message => {
-		message.innerHTML = decryptMessage(password, message.innerHTML);
+	const key = cryptoWrapper.deriveKey(password);
+	console.log(cryptoWrapper.toBase64(key));
+
+	$(".message").forEach(m => {
+		try {
+			const encryptedMessage = m.innerHTML;
+			console.log(encryptedMessage);
+			m.innerHTML = cryptoWrapper.decrypt(key, encryptedMessage);
+		} catch(error) {
+			console.log(error);
+		}
 	});
 }
 
-document.querySelector("#decrypt").onclick = () => {
+$("#decrypt")[0].onclick = () => {
 	const password = document.querySelector("#password").value;
 	localStorage.password = password;
 
 	decryptAllMessages(password);
 };
 
-if(localStorage.password) {
-	decryptAllMessages(localStorage.password);
-}
+decryptAllMessages(localStorage.password);
 
-document.querySelectorAll(".entry").forEach(entry => {
+$(".entry").forEach(entry => {
 	let visible = true;
 
 	entry.onclick = () => {
